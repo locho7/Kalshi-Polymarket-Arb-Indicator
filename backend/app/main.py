8741bbf66f5f0f3ae145b891e2fb95504ac78b28
Loc Ho <llocho2007@gmail.com>
2026-07-17 22:40:30 -0700

    e

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+from pydantic import BaseModel
 
 app = FastAPI()
+
+origins = [
+    "http://localhost:3000",
+    "http://localhost:5173", 
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+)
 
 opportunities = [
     {
