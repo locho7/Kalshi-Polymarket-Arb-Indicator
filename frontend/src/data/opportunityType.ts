a589982835e8f3480160107eb4f1ac9d0572bacb
Loc Ho <llocho2007@gmail.com>
2026-07-17 22:40:48 -0700

    create opportunity types file

---
+++ frontend/src/data/opportunityType.ts
@@ -0,0 +1,10 @@
+export type OpportunityType = {
+    id: number;
+    title: string;
+    category: string;
+    kalshiYes: number;
+    kalshiNo: number;
+    polymarketYes: number;
+    polymarketNo: number;
+    lastUpdated: string;
+}
