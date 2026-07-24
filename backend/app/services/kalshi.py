453f34da7846260123b8df1853e7d9ff16da0957
Loc Ho <llocho2007@gmail.com>
2026-07-23 19:43:57 -0700

    updated function to fetch by specific tag

--- backend/app/services/kalshi.py
+++ backend/app/services/kalshi.py
@@ -1,69 +1,19 @@
 import requests
 
-base_url = "https://external-api.kalshi.com/trade-api/v2"
-
-
-def fetch_all_active_markets_k(order="volume", ascending=False, active=True, limit=100):
-    url = f"{base_url}/markets"
-    params = {
-        "limit": limit,
-    }
-
-    if active:
-        params["status"] = "open"
-
-    response = requests.get(url, params=params)
-
+def fetch_event(event_ticker):
+    event_ticker = event_ticker.upper()
+    url = f"https://external-api.kalshi.com/trade-api/v2/events/{event_ticker}"
+    response = requests.get(url, params = {"with_nested_markets": True})
     if response.status_code == 200:
-        markets_data = response.json()
-        markets = markets_data["markets"]
-
-        if order == "volume":
-            markets.sort(
-                key=lambda market: float(market.get("volume_fp", 0)),
-                reverse=not ascending
-            )
-
-        for market in markets:
-            print(f"Title: {market['title']}")
-            print(f"Volume: {market['volume_fp']}")
-            print(f"Ticker: {market['ticker']}")
+        event_data = response.json()
+        for market in event_data["event"]["markets"]:
+            print(f" Ticker: {market["ticker"]}")
+            print(f" Title: {market["yes_sub_title"]}")
+            print(f" YES ask: {market["yes_ask_dollars"]}")
+            print(f" NO ask: {market["no_ask_dollars"]}")
             print()
-
-        return markets
-    else:
-        print(f"Error: {response.status_code}")
-        print(response.text)
-        return None
+        return event_data
+    print(f"Error: {response.status_code}")
 
 
-def get_markets():
-    url = "https://external-api.kalshi.com/trade-api/v2/markets"
-    params = {
-        "limit": 10,
-        "status": "open"
-    }
-
-    response = requests.get(url, params=params)
-
-    if response.status_code == 200:
-        markets_data = response.json()
-        print(markets_data)
-    else:
-        print(f"Error: {response.status_code}")
-
-def fetch_markets(series):
-    url = f"{base_url}/markets?series_ticker={series}&status=open"
-    response = requests.get(url)
-    if response.status_code == 200:
-        markets_data = response.json()
-        for market in markets_data['markets']:
-            print(f"- {market['title']}")
-            print(f"  Ticker: {market['event_ticker']}")
-            print(f"  Yes Price: ${market['yes_bid_dollars']} | No Price: {market['no_bid_dollars']}")
-            print(f"  Last Price Dollars: ${market['last_price_dollars']}")
-            print()
-            return market
-    return None
-
-fetch_all_active_markets_k()
+fetch_event("kxnextteamnba-26ljam")
--- backend/app/services/polymarket.py
+++ backend/app/services/polymarket.py
@@ -1,22 +1,21 @@
+import json
 import requests
 
-def fetch_all_active_markets_p():
-    url = "https://gamma-api.polymarket.com/markets"
-    params = {
-        "order": "volume",
-        "ascending": False,
-        "active": True,
-        "limit": 100,
-    }
-
-    response = requests.get(url, params=params)
+def fetch_event(slug):
+    url = f"https://gamma-api.polymarket.com/events/slug/{slug}"
+    response = requests.get(url, params = {"active": True})
     if response.status_code == 200:
-        markets_data = response.json()
-        for market in markets_data:
-            print(f"Question: {market['question']}")
-            print(f"Volume: {market['volume']}")
-            print(f"ID: {market['id']}")
-    else:
-        print(f"Error: {response.status_code}")
+        event_data = response.json()
+        for market in event_data["markets"]:
+            if "outcomePrices" not in market: continue
+            outcomePrices = json.loads(market["outcomePrices"])
+            print(f" - Market Ticker/ID : {market['id']}")
+            print(f"   Title: {market['question']}")
+            print(f"   Outcomes: {market['outcomes']}")
+            print(f"   YES ask: {outcomePrices[0]}")
+            print(f"   No ask: {outcomePrices[1]}")
+            print()
+        return event_data
+    print(f"Error: {response.status_code}")
 
-fetch_all_active_markets_p()
+fetch_event("nba-lebron-james-next-team")
