ee917a9715c387c4007ef37339935723384aa64a
Loc Ho <llocho2007@gmail.com>
2026-08-17 18:15:47 -0700

    added postgres to store market pairs and adjusted backend accordingly

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,8 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException, Depends
 from fastapi.middleware.cors import CORSMiddleware
-from pydantic import BaseModel
-from app.data.build_opportunities import build_opportunities
+
+from app import models
+from sqlalchemy import select
+from app.database import engine, get_db
+from sqlalchemy.orm import Session
+from typing import List, Annotated
+
+from app.opportunity_service import *
+
 app = FastAPI()
+models.Base.metadata.create_all(bind=engine)
+db_dependency = Annotated[Session, Depends(get_db)]
+
 
 origins = [
     "http://localhost:3000",
@@ -15,7 +25,37 @@ app.add_middleware(
     allow_credentials=True,
 )
 
-@app.get("/get-opportunities")
-def get_opportunities():
-    return build_opportunities()
+@app.get("/get-marketpairs")
+def read_market_pairs(db: db_dependency):
+    pairs = db.query(models.Pair).all()
+    return pairs
 
+
+@app.get("/get-opportunities")
+def get_opportunities(db: db_dependency):
+    market_pairs = db.query(models.Pair).all()
+    opportunities = build_opportunities(
+        market_pairs,
+        get_kalshi_events,
+        get_polymarket_slugs
+    )
+    return opportunities
+
+
+@app.post("/marketpair")
+async def create_market_pair (
+    pair: models.MarketPairBase,
+    db: db_dependency
+):
+   db_pair = models.Pair (
+       kalshi_event_ticker = pair.kalshi_event_ticker.upper(),
+       kalshi_market_ticker = pair.kalshi_market_ticker.upper(),
+       polymarket_slug = pair.polymarket_slug.lower(),
+       polymarket_id = pair.polymarket_id,
+   )
+
+   db.add(db_pair)
+   db.commit()
+   db.refresh(db_pair)
+
+   return db_pair
---
+++ backend/app/opportunity_service.py
@@ -0,0 +1,69 @@
+import json
+from app import models
+
+from sqlalchemy import select
+from sqlalchemy.orm import Session
+from typing import List, Annotated
+
+from datetime import datetime
+from operator import attrgetter
+from app.utils import *
+from app.market_service import (get_polymarket_market, 
+                                get_kalshi_market)
+
+def get_kalshi_events(db: Session):
+    stmt = select(models.Pair.kalshi_event_ticker).distinct()
+    return set(db.scalars(stmt).all())
+
+def get_polymarket_slugs(db: Session):
+    stmt = select(models.Pair.polymarket_slug).distinct()
+    return set(db.scalars(stmt).all())
+
+def build_opportunities(market_pairs, events, slugs):
+    opportunities = []
+    for pair in market_pairs:
+        kalshi_market = get_kalshi_market(
+            events,
+            pair.kalshi_event_ticker, 
+            pair.kalshi_market_ticker
+        )
+        polymarket_market = get_polymarket_market(
+            slugs,
+            pair.polymarket_slug,
+            pair.polymarket_id
+        )
+        
+        if kalshi_market is None or polymarket_market is None:
+            continue
+
+        k_yes = float(kalshi_market['yes_ask_dollars'])
+        k_no = float(kalshi_market['no_ask_dollars'])
+
+        outcome_prices = json.loads(polymarket_market['outcomePrices'])
+        p_yes = float(outcome_prices[0])
+        p_no = float(outcome_prices[1])
+
+        difference = find_price_difference(k_yes, k_no, p_yes, p_no)
+        if difference is None:
+            continue
+
+        trade = find_trade(k_yes, k_no, p_yes, p_no)
+
+        opportunities.append(models.OpportunityBase(
+            id=f"{kalshi_market['ticker']}:{polymarket_market['id']}",
+            title=f"{polymarket_market['question']}",
+            kalshi_market_ticker=kalshi_market['ticker'],
+            polymarket_slug=polymarket_market['slug'],
+            polymarket_id=polymarket_market['id'],
+            kalshi_yes_ask=k_yes,
+            kalshi_no_ask=k_no,
+            polymarket_yes_ask=p_yes,
+            polymarket_no_ask=p_no,
+            price_difference=difference,
+            best_trade=trade,
+            last_updated=datetime.now().strftime("%H:%M:%S")
+        ))
+
+    opportunities.sort(key=attrgetter('price_difference'), reverse=True)
+
+    return opportunities
